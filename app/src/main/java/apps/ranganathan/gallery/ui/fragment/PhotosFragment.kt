9768bf4e8576609d593f861af12f873464a9083a
Ranganathan <ranganathan@shamlatech.com>
2019-06-05 22:46:40 +0530

    fragment added

--- app/src/main/java/apps/ranganathan/gallery/ui/fragment/PhotosFragment.kt
+++ app/src/main/java/apps/ranganathan/gallery/ui/fragment/PhotosFragment.kt
@@ -24,6 +24,7 @@ class PhotosFragment : Fragment() {
 
     private lateinit var viewModel: PhotosViewModel
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
