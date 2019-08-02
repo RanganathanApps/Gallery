1451c7fcad580b4542c360b88dd2853e0c98f29a
RanganathanApps <apps.ranganathan@gmail.com>
2019-08-02 15:44:08 +0530

    fabric updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:3.4.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.40")
         classpath("com.google.gms:google-services:4.3.0")
-        classpath("io.fabric.tools:gradle:1.27.1")
+        classpath("io.fabric.tools:gradle:1.31.0")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
     }
